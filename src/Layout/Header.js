713aec5d5157be54fc31e3b64ba48d71a494aebc
Tushar kashyap <75391307+tusharkashyap2768@users.noreply.github.com>
2022-05-29 18:01:01 +0530

    Update Header.js

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -14,10 +14,11 @@ import { Link } from "react-router-dom";
 import { UserContext } from "../Context/UserContext";
 
 const Header = () => {
+  //this context is avaliable to us throughout
   const context = useContext(UserContext);
-
+   //state for toggle to be working
   const [isOpen, setIsOpen] = useState(false);
-
+   
   const toggle = () => setIsOpen(!isOpen);
 
   return (
@@ -28,12 +29,19 @@ const Header = () => {
         </Link>
       </NavbarBrand>
       <NavbarText className="text-white">
+      
+       // welcome username ka
         {context.user?.uid ? "welcome! " + context.user.email : ""}
       </NavbarText>
+       
       <NavbarToggler onClick={toggle} />
       <Collapse isOpen={isOpen} navbar>
         <Nav className="ml-auto" navbar>
-          {context.user ? (
+          
+          //if user is present then show logout otherwise show signup/in 
+          //conitional rendering
+          {context.user ? 
+           (
             <NavItem>
               <NavLink
                 onClick={() => {
