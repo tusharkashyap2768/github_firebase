4f6798f8295aefd7f7861ef3b13a0427ddcb0273
Tushar kashyap <75391307+tusharkashyap2768@users.noreply.github.com>
2022-06-01 23:47:04 +0530

    Update Repos.js

--- src/Components/Repos.js
+++ src/Components/Repos.js
@@ -3,19 +3,23 @@ import Axios from "axios";
 import { ListGroup, ListGroupItem } from "reactstrap";
 
 const Repos = ({ repos_url }) => {
+ // ([]) bcz we get array of repos
   const [repos, setRepos] = useState([]);
 
   const fetchRepos = async () => {
+    //extracting 
     const { data } = await Axios.get(repos_url);
+    // calling and setting data to states
     setRepos(data);
   };
-
+  // as soon as i grab url fretch repos()  start hojyga which will fetch everything into my state and fill the repos
   useEffect(() => {
-    fetchRepos();
-  }, [repos_url]);
+    fetchRepos(); 
+  }, [repos_url]); //trigger point
 
   return (
     <ListGroup>
+    //loop through repos list using map
       {repos.map((repo) => (
         <ListGroupItem key={repo.id}>
           <div className="text-secondary">{repo.name}</div>
